7df457c86a4b1f914ae2c71b2a97140c19bba613
Bakri1851 <70341270+Bakri1851@users.noreply.github.com>
2025-03-10 17:11:24 +0000

    feat: add wagmi integration for wallet connectivity and update package dependencies

---
+++ wagmi.js
@@ -0,0 +1,20 @@
+import {createClient, configureChains} from '@wagmi/core';
+import {mainnet, sepolia} from '@wagmi/chains';
+import {MetaMaskConnector} from '@wagmi/connectors';
+import {publicProvider} from '@wagmi/providers';
+
+const {chains, provider, webSocketProvider} = configureChains(
+    [mainnet,sepolia],
+    [publicProvider()]);
+
+const client = createClient({
+    autoConnect: true,
+    connectors: [
+        new MetaMaskConnector({chains}),
+        // Add more connectors here
+    ],
+    provider,
+    webSocketProvider,
+});
+
+export {client, chains}
